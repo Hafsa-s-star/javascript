910ba72ecf4769657d82d1d2442ffc31af91e16a
Hafsa Bilal <pakistan.hafsa@gmail.com>
2024-11-10 14:19:09 +0500

    Add files via upload

---
+++ assests/css/js/app.js
@@ -0,0 +1,39 @@
+alert("Thank You For Your Feedback")
+
+var firstname="Hafsa"
+
+console.log(firstname)
+
+console.log(firstname)
+
+console.log(firstname)
+
+console.log(firstname)
+
+console.log(firstname)
+
+console.log(firstname)
+
+var nationality="Pakistan"
+
+console.log(nationality)
+
+console.log(nationality)
+console.log(nationality)
+
+console.log(nationality)
+
+console.log(nationality)
+
+console.log(nationality)
+
+console.log(nationality)
+
+var floogle = "Hallie"
+
+console.log(floogle)
+console.log(floogle)
+console.log(floogle)
+console.log(floogle)
+console.log(floogle)
+console.log(floogle)
